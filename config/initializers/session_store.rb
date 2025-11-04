0e0b0baf861e64f0d3cae48655437568e7193059
shivam141102 <shivam2002mishra@gmail.com>
2025-11-04 15:06:42 -0600

    Fix lint issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,10 @@
 
 if Rails.env.production?
   Rails.application.config.session_store :active_record_store, {
-    key: '_csce606_group5_project1_session',
+    key: "_csce606_group5_project1_session",
     domain: :all,
     tld_length: 2
   }
 else
-  Rails.application.config.session_store :active_record_store, key: '_csce606_group5_project1_session'
+  Rails.application.config.session_store :active_record_store, key: "_csce606_group5_project1_session"
 end
