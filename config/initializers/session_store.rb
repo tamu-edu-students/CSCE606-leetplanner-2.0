1a7ea031a0f778994a8be3bb9b2e23ca26cb71e6
shreyasahni <shreyasahni7@gmail.com>
2025-11-04 18:37:26 -0600

    Fixed rails session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,14 +1,14 @@
 Rails.application.config.session_store :active_record_store
 
-# Configure all session options on config.action_dispatch.session_options
-# Start with common options for all environments.
-Rails.application.config.action_dispatch.session_options.update(
+# Configure all session options on config.action_dispatch.session
+# This hash will be used to create the session.
+Rails.application.config.action_dispatch.session = {
   key: "_csce606_group5_project1_session"
-)
+}
 
-# Add production-specific options
+# Add production-specific options by merging them into the hash
 if Rails.env.production?
-  Rails.application.config.action_dispatch.session_options.update(
+  Rails.application.config.action_dispatch.session.merge!(
     domain: :all,
     tld_length: 2
   )
