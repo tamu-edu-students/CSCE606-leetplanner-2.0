89f7e45da662c05547b8be698b2fd709234ca75a
Sharique <sharique@tamu.edu>
2025-11-04 19:13:43 -0600

    Moved message commit to db after_commit from after_create_commit - There might be a race condition in deployment between postgres and page refresh.

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,9 @@ class Message < ApplicationRecord
   # Validate presence of message body
   validates :body, presence: true
 
-  # Broadcast new messages to the lobby after create (ActionCable / Turbo Streams)
-  after_create_commit { broadcast_append_to lobby }
+  after_commit :broadcast_message, on: :create
+
+  def broadcast_message
+    broadcast_append_to lobby if lobby.present?
+  end
 end
