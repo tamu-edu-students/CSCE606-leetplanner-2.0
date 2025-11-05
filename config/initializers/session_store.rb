61b78c25194a140abfa6b7a28ab90bca76c1f1aa
Sharique <sharique@tamu.edu>
2025-11-04 19:21:07 -0600

    Changed session_store syntax to suit ruby 8 - Session store config for production contained old syntax changed it to match the rails 8 syntax.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,10 @@
 # Rails.application.config.session_store :active_record_store, key: "_csce606_group5_project1_session"
 
 if Rails.env.production?
-  Rails.application.config.session_store :active_record_store, {
-    key: "_csce606_group5_project1_session",
-    domain: :all,
-    tld_length: 2
-  }
+  Rails.application.config.session_store :active_record_store,
+      key: "_csce606_group5_project1_session",
+      domain: :all,
+      tld_length: 2
 else
   Rails.application.config.session_store :active_record_store, key: "_csce606_group5_project1_session"
 end
